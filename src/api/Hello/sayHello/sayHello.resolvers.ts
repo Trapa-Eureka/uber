85cead9870a8720f4c00fbe8c35d5a62519496c5
trapa <1982gaya@gmail.com>
2019-09-30 18:59:04 +0900

    recover 7.TypeCheck Graphql Arguments

--- src/api/Hello/sayHello/sayHello.resolvers.ts
+++ src/api/Hello/sayHello/sayHello.resolvers.ts
@@ -1,11 +1,11 @@
-import { Greeting } from "src/types/graph";
+import { SayHelloQueryArgs, SayHelloResponse } from "src/types/graph";
 
 const resolvers = {
     Query: {
-        sayHello: () : Greeting => {
+        sayHello: (_, args: SayHelloQueryArgs): SayHelloResponse => {
             return {
                 error: false,
-                text: "full stack"
+                text: `Hello ${args.name}`
             };
         }
     }
