edd22222e4be88f6c17898fbd1c32d1d2096047f
trapa <1982gaya@gmail.com>
2019-09-30 17:41:05 +0900

    recover 5. API & Schema Structure

--- src/schema.ts
+++ src/schema.ts
@@ -1,19 +1,19 @@
-import { GraphQLSchema } from "graphql";
+// import { GraphQLSchema } from "graphql";
 import { makeExecutableSchema } from "graphql-tools";
 import { fileLoader, mergeResolvers, mergeTypes } from "merge-graphql-schemas";
 import path from "path";
 
 
-const allTypes: GraphQLSchema[] = fileLoader(
+const allTypes: any = fileLoader(
     path.join(__dirname, "./api/**/*.graphql")
 );
 
-const allResolvers: string[] = fileLoader(
+const allResolvers: any = fileLoader(
     path.join(__dirname, "./api/**/*.resolvers.*")
 );
 
-const mergedTypes = mergeTypes(allTypes);
-const mergedResolvers = mergeResolvers(allResolvers);
+const mergedTypes: any = mergeTypes(allTypes);
+const mergedResolvers: any = mergeResolvers(allResolvers);
 
 const schema = makeExecutableSchema({
     typeDefs: mergedTypes,
